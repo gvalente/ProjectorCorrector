9cc95ab6d709040265082c68c96281db9e87bbd4
Gino Valente <gino.valente3@gmail.com>
2018-01-19 11:16:25 -0800

    Cleaned up a bunch of messed up tab indenting that came from somewhere

--- Projector Corrector.sketchplugin/Contents/Sketch/library/utilities.js
+++ Projector Corrector.sketchplugin/Contents/Sketch/library/utilities.js
@@ -1,93 +1,93 @@
 var com = com || {};
 
 com.gino = {
-	overlayColor   : '#000000',
-	overlayAlpha   : 0.10,
-    overlayOffset  : 0.02,
-	overlayPadding : 0.25,
-	overlayName    : 'ProjectorCorrector',
-	context        : undefined,
-	document       : undefined,
-	selection      : undefined,
-	page           : undefined,
-    pages          : undefined,
-	artboard       : undefined,
-	current        : undefined,
+		overlayColor   : '#000000',
+		overlayAlpha   : 0.10,
+		overlayOffset  : 0.02,
+		overlayPadding : 0.25,
+		overlayName    : 'ProjectorCorrector',
+		context        : undefined,
+		document       : undefined,
+		selection      : undefined,
+		page           : undefined,
+		pages          : undefined,
+		artboard       : undefined,
+		current        : undefined,
 
-    init: function(context){
-        this.context = context;
-        this.document = context.document;
-        this.selection = context.selection;
-        this.page = this.document.currentPage();
-        this.pages = this.document.pages();
-        this.artboard = this.page.currentArtboard();
-        this.current = this.artboard || this.page;
-    },
-    extend: function( options, target ){
-        var target = target || this;
+		init: function(context){
+			  this.context = context;
+			  this.document = context.document;
+			  this.selection = context.selection;
+			  this.page = this.document.currentPage();
+			  this.pages = this.document.pages();
+			  this.artboard = this.page.currentArtboard();
+			  this.current = this.artboard || this.page;
+		},
+		extend: function( options, target ){
+			  var target = target || this;
 
-        for ( var key in options ){
-            target[key] = options[key];
-        }
-        return target;
-    },
-    is: function(layer, theClass){
-        if(!layer) return false;
-        var klass = [layer class];
-        return klass === theClass;
-    },
-    isIntersect: function(lf, tf){
-        return !(
-            lf.maxX <= tf.x ||
-            lf.x >= tf.maxX ||
-            lf.y >= tf.maxY ||
-            lf.maxY <= tf.y
-        );
-    },
-    getFrame: function(layer) {
-        var rect = layer.absoluteRect();
-        return {
-            x: Math.round(rect.x()),
-            y: Math.round(rect.y()),
-            width: Math.round(rect.width()),
-            height: Math.round(rect.height()),
-            maxX: Math.round(rect.x() + rect.width()),
-            maxY: Math.round(rect.y() + rect.height())
-        };
-    },
-    getDistance: function(frame, target){
-        var tf = target || this.getFrame(this.current);
+			  for ( var key in options ){
+			  		target[key] = options[key];
+			  }
+			  return target;
+		},
+		is: function(layer, theClass){
+			  if(!layer) return false;
+			  var klass = [layer class];
+			  return klass === theClass;
+		},
+		isIntersect: function(lf, tf){
+			  return !(
+					  lf.maxX <= tf.x ||
+					  lf.x >= tf.maxX ||
+					  lf.y >= tf.maxY ||
+					  lf.maxY <= tf.y
+			  );
+		},
+		getFrame: function(layer) {
+			  var rect = layer.absoluteRect();
+			  return {
+			      x: Math.round(rect.x()),
+			      y: Math.round(rect.y()),
+			      width: Math.round(rect.width()),
+			      height: Math.round(rect.height()),
+			      maxX: Math.round(rect.x() + rect.width()),
+			      maxY: Math.round(rect.y() + rect.height())
+			  };
+		},
+		getDistance: function(frame, target){
+		    var tf = target || this.getFrame(this.current);
 
-        return [
-            ( frame.y - tf.y ),
-            ( (tf.x + tf.width) - frame.maxX ),
-            ( (tf.y + tf.height) - frame.maxY ),
-            ( frame.x - tf.x )
-        ];
-    },
-    addLayer: function(type, container){
-        var container = container || this.current;
-        return container.addLayerOfType(type);
-    },
-    addGroup: function(container){
-        var container = container || this.current;
-        return this.addLayer("group", container);
-    },
-    addShape: function(container){
-        var container = container || this.current;
-        return this.addLayer("rectangle", container);
-    },
-    addText: function(container){
-        var container = container || this.current;
-        return this.addLayer("text", container);
-    },
-    removeLayer: function(layer){
-        var container = layer.parentGroup();
-        if (container) container.removeLayer(layer);
-    },
-    message: function(message){
-        this.document.showMessage(message);
-    }
+		    return [
+		        ( frame.y - tf.y ),
+		        ( (tf.x + tf.width) - frame.maxX ),
+		        ( (tf.y + tf.height) - frame.maxY ),
+		        ( frame.x - tf.x )
+		    ];
+		},
+		addLayer: function(type, container){
+		    var container = container || this.current;
+		    return container.addLayerOfType(type);
+		},
+		addGroup: function(container){
+		    var container = container || this.current;
+		    return this.addLayer("group", container);
+		},
+		addShape: function(container){
+		    var container = container || this.current;
+		    return this.addLayer("rectangle", container);
+		},
+		addText: function(container){
+		    var container = container || this.current;
+		    return this.addLayer("text", container);
+		},
+		removeLayer: function(layer){
+		    var container = layer.parentGroup();
+		    if (container) container.removeLayer(layer);
+		},
+		message: function(message){
+		    this.document.showMessage(message);
+		}
 };
 
 
@@ -101,8 +101,7 @@ com.gino.extend({
 
         if(isArray){
             items = container;
-        }
-        else{
+        } else {
             items = container.children();
         }
 
@@ -121,123 +120,123 @@ com.gino.extend({
 
 // Helper Functions
 com.gino.extend({
+		// Cleaner iteration function
+		iterateObjects: function(collection, iterator) {
+		    for (var i = 0; i < collection.count(); i++) {
+		        var object = collection.objectAtIndex(i);
+		        var returnValue = iterator(object, i, collection);
+		        if (returnValue === false) {
+		            break;
+		        }
+		    }
+		},
 
-    // cleaner iteration function
-    iterateObjects: function(collection, iterator) {
-        for (var i = 0; i < collection.count(); i++) {
-            var object = collection.objectAtIndex(i);
-            var returnValue = iterator(object, i, collection);
-            if (returnValue === false) {
-                break;
-            }
-        }
-    },
+		// add a color and alpha fill to a shape
+		addFillToShape: function(shape, color) {
+				// Check for existing fill and add
+				var fill = shape.style().addStylePartOfType(0);
 
-    // add a color and alpha fill to a shape
-	addFillToShape: function(shape, color) {
+				// add color to fill
+				fill.color = MSImmutableColor.colorWithSVGString(color);
+		},
 
-        // Check for existing fill and add
-		var fill = shape.style().addStylePartOfType(0);
+		// get artboard bounds of a page
+		getArtboardBounds: function(page) {
+		    // Normalize the padding based on width/height then max at 200 on each side
+		    var pageBounds = MSLayerGroup.groupBoundsForContainer(MSLayerArray.arrayWithLayers(page));
+		    var unifiedPadding = Math.max(pageBounds.size.width, pageBounds.size.height) * this.overlayPadding;
+		    unifiedPadding = Math.min(unifiedPadding, 400);
 
-		// add color to fill
-		fill.color = MSImmutableColor.colorWithSVGString(color);
-	},
+				return NSMakeRect(
+					+pageBounds.origin.x    - unifiedPadding / 2,
+					+pageBounds.origin.y    - unifiedPadding / 2,
+					+pageBounds.size.width  + unifiedPadding,
+					+pageBounds.size.height + unifiedPadding
+				);
+		},
 
-	// get artboard bounds of a page
-	getArtboardBounds: function(page) {
-        // Normalize the padding based on width/height then max at 200 on each side
-        var pageBounds = MSLayerGroup.groupBoundsForContainer(MSLayerArray.arrayWithLayers(page));
-        var unifiedPadding = Math.max(pageBounds.size.width, pageBounds.size.height) * this.overlayPadding;
-        unifiedPadding = Math.min(unifiedPadding, 400);
+		// check if PCoverlay is present on any page
+		overlayExists: function(container) {
+				var container = container || this.pages;
 
-		return NSMakeRect(
-			+pageBounds.origin.x    - unifiedPadding / 2,
-			+pageBounds.origin.y    - unifiedPadding / 2,
-			+pageBounds.size.width  + unifiedPadding,
-			+pageBounds.size.height + unifiedPadding
-		);
-	},
+				// Single Page
+				if(container instanceof MSPage) {
+						return this.find( this.overlayName, container );
+				}
 
+				// Multiple Pages
+				var exists = false;
+				this.iterateObjects(container, function(childPage) {
+						//TODO: Check if duplicate layers exist on same page
+						if( com.gino.find(com.gino.overlayName, childPage) ) exists = true;
+				});
 
-    // check if PCoverlay is present on any page
-    overlayExists: function(container) {
-        var container = container || this.pages;
+				return exists;
+		},
 
-        // Single Page
-        if( container instanceof MSPage ) {
-            return this.find( this.overlayName, container );
-        }
-
-        // Multiple Pages
-        var exists = false;
-        this.iterateObjects(container, function(childPage) {
-            //TODO: Check if duplicate layers exist on same page
-            if( com.gino.find(com.gino.overlayName, childPage) ) exists = true;
-        });
-
-        return exists;
-    },
-    getOverlay: function(container) {
-        var container = container || this.page;
-        return this.find( this.overlayName, container );
-    }
+		getOverlay: function(container) {
+				var container = container || this.page;
+				return this.find( this.overlayName, container );
+		}
 });
 
 
 // Overlay
 com.gino.extend({
-    createOverlay: function(page) {
-        var rectShape = MSRectangleShape.alloc().initWithFrame( this.getArtboardBounds( page.artboards() ) );
+		createOverlay: function(page) {
+				var rectShape = MSRectangleShape.alloc().initWithFrame( this.getArtboardBounds( page.artboards() ) );
 
-        var overlay = MSShapeGroup.shapeWithPath(rectShape);
-            overlay.isLocked = true;
-            overlay.name = this.overlayName;
-            overlay.style().contextSettings().opacity = this.overlayAlpha;
+				var overlay = MSShapeGroup.shapeWithPath(rectShape);
+				overlay.isLocked = true;
+				overlay.name = this.overlayName;
+				overlay.style().contextSettings().opacity = this.overlayAlpha;
 
-        this.addFillToShape( overlay, this.overlayColor );
-        return [overlay];
-    },
-	addOverlay: function() {
-        this.iterateObjects(this.pages, function(childPage) {
-            //TODO: Skip if no artboards on page
-            childPage.addLayers( com.gino.createOverlay(childPage) );
-        });
-	},
-    removeOverlay: function() {
-        this.iterateObjects(this.pages, function(childPage) {
-            com.gino.removeLayer( com.gino.find(com.gino.overlayName, childPage) );
-        });
-    },
-    // check for existence of overlay and either add or remove
-    toggleOverlay: function() {
-        if( !this.overlayExists() ) {
-            this.addOverlay();
-        } else {
-            this.removeOverlay();
-        }
-    },
-    // Offset overlay alpha based on operator
-    offsetOverlayAlpha: function(op) {
-        if( !this.overlayExists() ) return;
+				this.addFillToShape( overlay, this.overlayColor );
+				return [overlay];
+		},
+		addOverlay: function() {
+				this.iterateObjects(this.pages, function(childPage) {
+						//TODO: Skip if no artboards on page
+						childPage.addLayers( com.gino.createOverlay(childPage) );
+				});
+		},
+		removeOverlay: function() {
+				this.iterateObjects(this.pages, function(childPage) {
+						com.gino.removeLayer( com.gino.find(com.gino.overlayName, childPage) );
+				});
+		},
 
-        // Check new opacity and whether it is between 0 and 1
-        var operators = {
-            '+': function(overlay, opacityChange) {
-                    var newOpacity = ( overlay.style().contextSettings().opacity() + opacityChange ).toFixed(2);
-                    return (newOpacity <= 1) ? newOpacity : false;
-            },
-            '-': function(overlay, opacityChange) {
-                    var newOpacity = ( overlay.style().contextSettings().opacity() - opacityChange ).toFixed(2);
-                    return (newOpacity >= 0) ? newOpacity : false;
-            }
-        }
+		// check for existence of overlay and either add or remove
+		toggleOverlay: function() {
+				if( !this.overlayExists() ) {
+						this.addOverlay();
+				} else {
+						this.removeOverlay();
+				}
+		},
 
-        this.iterateObjects(this.pages, function(childPage) {
-            const overlay = com.gino.getOverlay( childPage );
+		// Offset overlay alpha based on operator
+		offsetOverlayAlpha: function(op) {
+				if( !this.overlayExists() ) return;
 
-            // get new opacity and apply if a valid increment
-            const newOpacity = operators[op](overlay, com.gino.overlayOffset);
-            if(newOpacity) { overlay.style().contextSettings().opacity = newOpacity; }
-        });
-    }
+				// Check new opacity and whether it is between 0 and 1
+				var operators = {
+						'+': function(overlay, opacityChange) {
+								var newOpacity = ( overlay.style().contextSettings().opacity() + opacityChange ).toFixed(2);
+								return (newOpacity <= 1) ? newOpacity : false;
+						},
+						'-': function(overlay, opacityChange) {
+								var newOpacity = ( overlay.style().contextSettings().opacity() - opacityChange ).toFixed(2);
+								return (newOpacity >= 0) ? newOpacity : false;
+						}
+				}
+
+				this.iterateObjects(this.pages, function(childPage) {
+						const overlay = com.gino.getOverlay( childPage );
+
+						// get new opacity and apply if a valid increment
+						const newOpacity = operators[op](overlay, com.gino.overlayOffset);
+						if(newOpacity) { overlay.style().contextSettings().opacity = newOpacity; }
+				});
+		}
 });
